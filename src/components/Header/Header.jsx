fe5709dfb4f50be133c6585b001767fc625c225d
Belov Dmitrii <belovd90@mail.ru>
2023-08-26 21:01:03 +0300

    added styles to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,37 +8,40 @@ import styles from '../../styles/Header.module.css';
 
 const Header = () => {
   return(
-    <div className={styles.header} style={{display:'flex'}}>
+    <div className={styles.header}>
       <div className={styles.logo}>
         <Link to={ROUTES.HOME}>
-          <img style={{width:"100px", height:"100px"}} src={LOGO} alt='logo' />
+          <img src={LOGO} alt='logo' />
         </Link>
       </div>
-      <div className={styles.mainInfo} style={{display:'flex'}}>
+      <div className={styles.mainInfo}>
         <div className={styles.user}>
           <div 
             className={styles.avatar}
-            style={{ backgroundImage: `url('${AVATAR}')`, width:"50px", height:"50px" }}
-          />
-        </div>
-        <div className={styles.userName}>
-          UserName
-        </div>
-        <form className={styles.form}>
-          <div className={styles.icon}>
-            <SearchIcon />
+          >
+            <img src={AVATAR} alt="avatar" />
           </div>
-          <div className={styles.input}>
-            <input 
-              type="search"
-              name="search"
-              placeholder="Поиск товара..."
-              autoComplete="off"
-              onChange={() => {}}
-              value=""
-            />
+        </div>
+        <div className={styles.userInfo}>
+          <div className={styles.userName}>
+            UserName
           </div>
-        </form>
+          <form className={styles.form}>
+            <div className={styles.icon}>
+              <SearchIcon />
+            </div>
+            <div className={styles.input}>
+              <input 
+                type="search"
+                name="search"
+                placeholder="Поиск товара..."
+                autoComplete="off"
+                onChange={() => {}}
+                value=""
+              />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   )
