b03e61334d812e160a95e4487f73f5ff0fc89325
Belov Dmitrii <belovd90@mail.ru>
2023-09-03 19:39:35 +0300

    added price ilter

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Poster from '../Poster/Poster';
 import Products from '../Products/Products';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Categories from '../Categories/Categories';
 import Banner from '../Banner/Banner';
+import { filterByPrice } from '../../features/products/productsSlice';
 
 const Home = () => {
-  const { products, categories } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const { products: {list, filtered}, categories } = useSelector((state) => state);
+
+  useEffect(() => {
+    if(!list.length) return;
+
+    dispatch(filterByPrice(15));
+  }, [dispatch, list.length])
   return (
     <>
       <Poster />
-      <Products products={products.list} amount={8} title="Trending"/>
+      <Products products={list} amount={8} title="Trending"/>
       <Categories products={categories.list} amount={4} title="You must see THIS"/>
       <Banner />
+      <Products products={filtered} amount={4} title="Trending"/>
     </>
   )
 }
