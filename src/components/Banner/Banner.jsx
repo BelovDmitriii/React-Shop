0bf0e5efe3bb40d237b40f0b41eb3e41cfb1b76a
Belov Dmitrii <belovd90@mail.ru>
2023-09-03 16:56:19 +0300

    addad styles to banner component

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import styles from '../../styles/Banner.module.css';
+import saleImage from '../../images/sale.jpg';
 
 const Banner = () => {
   return(
-    <div>Banner</div>
+    <section className={styles.banner}>
+      <div className={styles.left}>
+        <p className={styles.content}>
+          NEW SALES
+          <span>SALE</span>
+        </p>
+        <button className={styles.more}>See more</button>
+      </div>
+
+      <div className={styles.right}>
+        <img src={saleImage} alt="sale_photo" />
+        <p className={styles.discount}>
+          save up to <span>50%</span> off
+        </p>
+      </div>
+
+    </section>
   );
 }
 
